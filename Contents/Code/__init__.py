cba6c80fc9fb4ec6b43e7ea7fc06600be520b0db
Mike Goldsmith <goldsmith.mike@gmail.com>
2014-12-27 16:42:28 +0000

    Removed Default.json, added DefaultPrefs.json and sample Info.plist

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,1 +1,20 @@
-__author__ = 'mike'
+import re
+
+
+class Channel:
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+
+def Start():
+
+    menu = ObjectContainer();
+
+
+channel = Channel("mike", "http://shoof.alkass.net/shoof/live/live_1.php")
+channels = [channel]
+
+for c in channels:
+    print(c.name)
+    print(c.url)
